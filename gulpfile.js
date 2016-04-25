bb6a80b92d1c373eb37c774c81bf3e24a2d607a1
Piotr Kulpinski <piotr@kulpinski.pl>
2016-04-25 11:23:56 +0200

    Remove unnecessary code from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,46 @@
 'use strict';
 
-var gulp    = require('gulp');
-var plugins = require('gulp-load-plugins')({ pattern: '*' });
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var scssLint      = require('gulp-scss-lint');
+var plumber       = require('gulp-plumber');
+var postcss       = require('gulp-postcss');
+var autoprefixer  = require('autoprefixer');
+var browserSync   = require('browser-sync');
 
-var config  = require('./package.json').config;
-
-var styles = [
-  './styles/**/*.scss',
-  './contrib/main.scss'
-];
+var paths = {
+  styles: [
+    './styles/**/*.scss',
+    './contrib/main.scss'
+  ]
+};
 
 /**
 ** Styles task
 **/
 gulp.task('styles', function () {
-  return gulp.src(styles)
-    .pipe(plugins.plumber(function (error) {
+  return gulp.src(paths.styles)
+    .pipe(plumber(function (error) {
       console.error(error.message + '\n');
       console.error(error.fileName + ':' + error.lineNumber + '\n');
 
       this.emit('end');
     }))
-    .pipe(plugins.scssLint())
-    .pipe(plugins.sass({ outputStyle: 'expanded' }))
-    .pipe(plugins.postcss([
-      require('autoprefixer')({
-        flexbox: false,
-        browsers: ['last 2 versions']
-      })
-    ]))
+    .pipe(scssLint())
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
     .pipe(gulp.dest('./contrib'))
-    .pipe(plugins.browserSync.stream());
+    .pipe(browserSync.stream());
 });
 
 /**
 ** Default task
 **/
 gulp.task('default', function () {
-  plugins.browserSync.init({
+  browserSync.init({
     server: './contrib/'
   });
 
-  gulp.watch(styles, ['styles']);
-  gulp.watch('./contrib/*.html', plugins.browserSync.reload);
+  gulp.watch(paths.styles, ['styles']);
+  gulp.watch('./contrib/*.html', browserSync.reload);
 });
